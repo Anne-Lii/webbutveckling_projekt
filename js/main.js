f2a1fd7ee949a671930b4fd65b1093d23605585e
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2023-10-26 21:21:23 +0200

    fixat sidan för skärmar upp till 900px

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 "use strict";
+
+//hämtar element från  HTML-kod
+const openMenuButton = document.getElementById("openMenu");
+const closeMenuButton = document.getElementById("closeMenu");
+
+//eventlistener
+openMenuButton.addEventListener("click", menuToggle);
+closeMenuButton.addEventListener("click", menuToggle);
+
+//funktion som kollar om stylingen på menyn är display none eller block och ändrar vid klick
+function menuToggle() {
+
+    const navMenuDropEl = document.getElementById("navMenuDrop");
+
+    let style = window.getComputedStyle(navMenuDropEl);
+
+    if (style.display === "none") {
+        navMenuDropEl.style.display = "block";
+    } else {
+        navMenuDropEl.style.display = "none";
+    }
+
+}
